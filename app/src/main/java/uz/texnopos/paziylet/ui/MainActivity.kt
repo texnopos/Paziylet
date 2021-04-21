d1c8a5fc699cbc0b2c48b2006d2e3308fab97dbd
Sharapat Kalabaev <kalabaevshj@gmail.com>
2021-04-22 00:50:31 +0500

    fixed

--- app/src/main/java/uz/texnopos/paziylet/ui/MainActivity.kt
+++ app/src/main/java/uz/texnopos/paziylet/ui/MainActivity.kt
@@ -4,17 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 import uz.texnopos.paziylet.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navHostController: NavController
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navHostController = findNavController(R.id.fragmentContainer)
-        NavigationUI.setupWithNavController(bottomNavigation, navHostController)
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment).navController
     }
 }
---
+++ app/src/main/java/uz/texnopos/paziylet/ui/home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package uz.texnopos.paziylet.ui.home
+
+import androidx.fragment.app.Fragment
+import uz.texnopos.paziylet.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
