d12e9b0085aecad1ed1d81f568c88f309bfae99e
Zigfruid <Zigfruid@gmail.com>
2021-05-05 20:03:15 +0500

    compass was done

--- app/src/main/java/uz/texnopos/paziylet/ui/compass/CompassFragment.kt
+++ app/src/main/java/uz/texnopos/paziylet/ui/compass/CompassFragment.kt
@@ -1,117 +1,161 @@
 package uz.texnopos.paziylet.ui.compass
 
-import android.content.Context.SENSOR_SERVICE
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.*
-import android.os.Build
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
-import android.os.Handler
-import android.text.format.Time
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import androidx.annotation.RequiresApi
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.LocationServices
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import kotlinx.android.synthetic.main.compass_fragment.*
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 import uz.texnopos.paziylet.R
-import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.math.roundToInt
 
 
-class CompassFragment: Fragment(R.layout.compass_fragment) , SensorEventListener{
-    private lateinit  var sensorManager : SensorManager
+class CompassFragment: Fragment(R.layout.compass_fragment), EasyPermissions.PermissionCallbacks {
 
 
-    private lateinit var sensor : Sensor
-    private lateinit var magnetometerSensor: Sensor
-    private var mGravity = FloatArray(3)
-    private var mGeometric = FloatArray(3)
-    private var azimuth = 0f
-    private var currentAzimuth : Float = 0f
+    companion object {
+        const val TAG = "MainActivity"
+        const val QIBLA_LATITUDE = 21.3891
+        const val QIBLA_LONGITUDE = 39.8579
+    }
+
+    var currentDegree: Float = 0f
+    var currentNeedleDegree: Float = 0f
+    lateinit var sensorManager: SensorManager
+    lateinit var sensor: Sensor
+    lateinit var userLocation: Location
+    lateinit var needleAnimation: RotateAnimation
 
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sensorManager = activity?.getSystemService(SENSOR_SERVICE) as SensorManager
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-            getDate()
-    }
-
-    private fun getDate(){
-        val time = Time(Time.getCurrentTimezone())
-        time.setToNow()
-        tvDate.text = time.format("%Y.%m.%d")
-    }
-
-
-    @OptIn(InternalCoroutinesApi::class)
-    override fun onSensorChanged(event: SensorEvent?) {
-        val alpha= 0.97f
-        synchronized(this){
-            if (event != null) {
-                if (event.sensor.type == Sensor.TYPE_ACCELEROMETER){
-                    mGravity[0] = alpha*mGravity[0]+(1-alpha)*event.values[0]
-                    mGravity[1] = alpha*mGravity[1]+(1-alpha)*event.values[1]
-                    mGravity[2] = alpha*mGravity[2]+(1-alpha)*event.values[2]
-                }
-
-                if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD){
-                    mGeometric[0] = alpha*mGeometric[0]+(1-alpha)*event.values[0]
-                    mGeometric[1] = alpha*mGeometric[1]+(1-alpha)*event.values[1]
-                    mGeometric[2] = alpha*mGeometric[2]+(1-alpha)*event.values[2]
-                }
-
-                val R = FloatArray(9)
-                val I = FloatArray(9)
-                val success : Boolean = SensorManager.getRotationMatrix(R, I, mGravity, mGeometric)
-                if (success){
-                    val orientation = FloatArray(3)
-                    SensorManager.getOrientation(R, orientation)
-                    azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
-                    azimuth = (azimuth+360)%360
-                    val anim = RotateAnimation(-currentAzimuth, -azimuth, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-                    currentAzimuth = azimuth
-                    anim.duration = 500
-                    anim.repeatCount = 0
-                    anim.fillAfter = true
-                    ivCompass.startAnimation(anim)
-                    val x = azimuth
-                    tvDegree.text = "$x degrees"
-                }
-            }
+        needleAnimation = RotateAnimation(
+                currentNeedleDegree,
+                0f,
+                Animation.RELATIVE_TO_SELF,
+                .5f,
+                Animation.RELATIVE_TO_SELF,
+                .5f
+        )
+        if (hasLocationPermission()) {
+            initLocationListener()
+        } else {
+            requestLocationPermission()
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME)
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME)
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    private fun initLocationListener() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            Log.d(TAG, "User Location : Lat : ${it.latitude} Long : ${it.longitude}")
+            initQiblaDirection(it.latitude, it.longitude)
+            tvRegion.text = getCountryName(requireContext(), it.latitude, it.longitude)
+        }
+        fusedLocationClient.lastLocation.addOnFailureListener {
+            Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initQiblaDirection(latitude: Double, longitude: Double) {
+        userLocation = Location("User Location")
+        userLocation.latitude = latitude
+        userLocation.longitude = longitude
+
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onSensorChanged(sensorEvent: SensorEvent?) {
+                val degree: Float = sensorEvent?.values?.get(0)?.roundToInt()?.toFloat()!!
+                var head: Float = sensorEvent.values?.get(0)?.roundToInt()?.toFloat()!!
+                val destLocation = Location("Destination Location")
+                destLocation.latitude = QIBLA_LATITUDE
+                destLocation.longitude = QIBLA_LONGITUDE
+                var bearTo = userLocation.bearingTo(destLocation)
+                val geoField = GeomagneticField(
+                        userLocation.latitude.toFloat(),
+                        userLocation.longitude.toFloat(),
+                        userLocation.altitude.toFloat(),
+                        System.currentTimeMillis()
+                )
+                head -= geoField.declination
+                if (bearTo < 0) {
+                    bearTo += 360
+                }
+                var direction = bearTo - head
+                if (direction < 0) {
+                    direction += 360
+                }
+                needleAnimation = RotateAnimation(
+                        currentNeedleDegree,
+                        direction,
+                        Animation.RELATIVE_TO_SELF,
+                        0.5f,
+                        Animation.RELATIVE_TO_SELF,
+                        0.5f
+                )
+                needleAnimation.fillAfter = true
+                needleAnimation.duration = 200
+                ivStrelka.startAnimation(needleAnimation)
+                currentNeedleDegree = direction
+                currentDegree = -degree
+            }
+        }, sensor, SensorManager.SENSOR_DELAY_GAME)
 
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    private fun hasLocationPermission() =
+            EasyPermissions.hasPermissions(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private fun requestLocationPermission() {
+        EasyPermissions.requestPermissions(this, getString(R.string.dont_work_without_permission), 1,
+                Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionDenied(this, perms.first())) {
+            SettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestLocationPermission()
+        }
     }
 
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Toast.makeText(requireContext(), getString(R.string.permission_granted), Toast.LENGTH_SHORT).show()
+    }
 
-//    override fun onSensorChanged(event: SensorEvent?) {
-//        val degree = event?.values!![0].roundToInt()
-//        val animation = RotateAnimation(0F, (-degree).toFloat(), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-//        animation.duration = 500
-//        animation.fillAfter = true
-//        ivStrelka.animation = animation
-//        currentDegree=-degree.toFloat()
-//    }
-//
-//    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-//    }
-
+    private fun getCountryName(context: Context?, latitude: Double, longitude: Double): String {
+        val geoCoder = Geocoder(context, Locale.getDefault())
+        val addresses: List<Address> = geoCoder.getFromLocation(latitude, longitude, 1)
+        return "${addresses[0].locality!!} , ${addresses[0].countryName!!}"
+    }
 }
