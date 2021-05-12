849c9cd40ed17b14d69def2a4647b0eede141121
texnopos <texnopos@gmail.com>
2021-05-12 14:06:23 +0500

    location fragment

--- app/src/main/java/uz/texnopos/paziylet/ui/compass/CompassFragment.kt
+++ app/src/main/java/uz/texnopos/paziylet/ui/compass/CompassFragment.kt
@@ -1,30 +1,18 @@
 package uz.texnopos.paziylet.ui.compass
 
-import android.Manifest
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.content.IntentSender
-import android.content.pm.PackageManager
 import android.hardware.*
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
-import android.location.LocationManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.compass_fragment.*
 import uz.texnopos.paziylet.R
+import uz.texnopos.paziylet.core.ResourceState
+import uz.texnopos.paziylet.core.extentions.visibility
 import uz.texnopos.paziylet.ui.location.LocationFragment
 import java.text.SimpleDateFormat
 import java.util.*
@@ -42,50 +30,26 @@ class CompassFragment : LocationFragment(R.layout.compass_fragment) {
     private lateinit var sensor: Sensor
     lateinit var userLocation: Location
     lateinit var needleAnimation: RotateAnimation
-    private val trackingInterval: Long = 2000
-    private val locationRequest: LocationRequest = LocationRequest.create().apply {
-        interval = trackingInterval
-        fastestInterval = trackingInterval / 2
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tvDate.text = getCurrentDateAndTime().toString()
         location.observe(viewLifecycleOwner, {
-            it?.let { location ->
-                initQiblaDirection(location.latitude, location.longitude)
-                tvRegion.text = getCountryName(requireContext(), it.latitude, it.longitude)
-            }
-        })
-    }
-
-    private fun checkGpsStatus() {
-        val settingsClient: SettingsClient = LocationServices.getSettingsClient(requireActivity())
-        val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(locationRequest)
-        val locationManager =
-            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val locationSettingsRequest: LocationSettingsRequest = builder.build()
-        builder.setAlwaysShow(true)
-
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            settingsClient
-                .checkLocationSettings(locationSettingsRequest)
-                .addOnSuccessListener(context as Activity) {
-
+            when (it.status) {
+                ResourceState.LOADING -> {
+                    progressBar.visibility(true)
                 }
-                .addOnFailureListener(requireActivity()) { e ->
-                    when ((e as ApiException).statusCode) {
-                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->
-                            try {
-                                val rae = e as ResolvableApiException
-                                rae.startResolutionForResult(requireActivity(), 101)
-                            } catch (sie: IntentSender.SendIntentException) {
-                            }
+                ResourceState.SUCCESS -> {
+                    it.data?.let { location ->
+                        progressBar.visibility(false)
+                        initQiblaDirection(location.latitude, location.longitude)
+                        tvRegion.text =
+                            getCountryName(requireContext(), location.latitude, location.longitude)
                     }
                 }
-        }
+                else -> {}
+            }
+        })
     }
 
     private fun initQiblaDirection(latitude: Double, longitude: Double) {
@@ -142,66 +106,11 @@ class CompassFragment : LocationFragment(R.layout.compass_fragment) {
         return "${addresses[0].locality} , ${addresses[0].countryName}"
     }
 
-    private fun showDialog() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.apply {
-            setMessage(getString(R.string.permission_is_required))
-            setTitle(getString(R.string.permission_required_title))
-            setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
-                dialog.dismiss()
-            }
-            setPositiveButton(getString(R.string.go_to_settings)) { _, _ ->
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri: Uri = Uri.fromParts("package", activity?.packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }
-        }.create().show()
-    }
-
-    private fun checkForPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                != PackageManager.PERMISSION_GRANTED
-            ) {
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), FINE_LOCATION)
-            } else {
-                checkGpsStatus()
-            }
-        } else checkGpsStatus()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        fun innerCheck() {
-            if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                showDialog()
-            } else {
-                checkGpsStatus()
-            }
-        }
-        when (requestCode) {
-            FINE_LOCATION -> innerCheck()
-        }
-    }
-
     private fun getCurrentDateAndTime(): Any {
         val c = Calendar.getInstance().time
         val simpleDateFormat =
             SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(Calendar.getInstance().time)
                 .toString()
         return simpleDateFormat.format(c)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        checkForPermissions()
     }
 }
--- app/src/main/java/uz/texnopos/paziylet/ui/questions/category/QuestionCategoriesFragment.kt
+++ app/src/main/java/uz/texnopos/paziylet/ui/questions/category/QuestionCategoriesFragment.kt
@@ -9,8 +9,8 @@ import kotlinx.android.synthetic.main.fragment_questions_categories.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import uz.texnopos.paziylet.R
+import uz.texnopos.paziylet.core.ResourceState
 import uz.texnopos.paziylet.core.extentions.visibility
-import uz.texnopos.paziylet.di.ResourceState
 
 class QuestionCategoriesFragment : Fragment(R.layout.fragment_questions_categories) {
     private val viewModel: QuestionsCategoriesViewModel by viewModel()
