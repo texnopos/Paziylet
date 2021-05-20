5f75c4843af3d230abc4d152d76290709b501e95
UBatir <urazbaaev.br@gmail.com>
2021-05-20 16:43:14 +0500

    added webview

--- app/src/main/java/uz/texnopos/paziylet/ui/categories/definitecategory/DefiniteCategoryFragment.kt
+++ app/src/main/java/uz/texnopos/paziylet/ui/categories/definitecategory/DefiniteCategoryFragment.kt
@@ -3,6 +3,8 @@ package uz.texnopos.paziylet.ui.categories.definitecategory
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_category.*
 import org.koin.android.ext.android.inject
@@ -11,18 +13,24 @@ import org.koin.android.viewmodel.ext.android.viewModel
 import uz.texnopos.paziylet.core.ResourceState
 import uz.texnopos.paziylet.core.extentions.visibility
 
-class DefiniteCategory:Fragment(R.layout.fragment_category) {
+class DefiniteCategoryFragment:Fragment(R.layout.fragment_category) {
 
     private val viewModel:DefiniteCategoryViewModel by viewModel()
     private val adapter:DefiniteCategoryAdapter by inject()
-    private val safeArgs:DefiniteCategoryArgs by navArgs()
+    private val safeArgs:DefiniteCategoryFragmentArgs by navArgs()
+    lateinit var navController: NavController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController=Navigation.findNavController(view)
         recyclerViewCategory.adapter=adapter
         tvTitle.text=safeArgs.path
         viewModel.getData(safeArgs.path)
         setUpObserver()
+        adapter.onItemClickListener {
+            val action=DefiniteCategoryFragmentDirections.actionDefiniteCategoryToWebViewFragment(it)
+            navController.navigate(action)
+        }
     }
 
     private fun setUpObserver() {
---
+++ app/src/main/java/uz/texnopos/paziylet/ui/categories/definitecategory/webview/WebViewFragment.kt
@@ -0,0 +1,20 @@
+package uz.texnopos.paziylet.ui.categories.definitecategory.webview
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_webview.*
+import uz.texnopos.paziylet.R
+
+class WebViewFragment:Fragment(R.layout.fragment_webview) {
+
+    private val safeArgs:WebViewFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webView.loadData(safeArgs.text,"text/html","UTF-8")
+    }
+
+
+}
